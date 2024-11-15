ae730c4d3f942add19461e1fb0df4c42bd276b0c
Thomas Remmery <thomas.remmery@skyline.be>
2024-11-15 13:11:49 +0100

    Add summaries to protocol class.

--- TMDMediaFlexUMS/TMDMediaFlexUMSProtocol.cs
+++ TMDMediaFlexUMS/TMDMediaFlexUMSProtocol.cs
@@ -1,11 +1,18 @@
 ﻿namespace Skyline.DataMiner.ConnectorAPI.TMDMediaFlexUMS
 {
+    /// <summary>
+    /// Defines some of the key Parameter Ids used for communicating with a TMD MediaFlex UMS element running the 2.0.0.X range.
+    /// </summary>
     public static class TMDMediaFlexUMSProtocol
     {
-        public static readonly string Name = "";
-
+        /// <summary>
+        /// ID of the read parameter containing the Timeout value in seconds used for all InterApp communication to the element.
+        /// </summary>
         public static readonly int InterAppTimeoutPid = 250;
 
+        /// <summary>
+        /// ID of the read parameter to on which all InterApp messages are received.
+        /// </summary>
         public static readonly int InterAppReceivePid = 9000000;
     }
 }
